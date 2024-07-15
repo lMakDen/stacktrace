536655dd6cc2029195f2bc776b0f4944478c43df
Zolotukhin <zolotukhin.ivan@airslate.com>
2024-07-15 11:15:22 +0300

    Updates

--- src/components/Button.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  export let onClick: () => void;
-</script>
-
-<button on:click={onClick}>Decrypt</button>
--- src/components/TextArea.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  export let onChange: ({ target }: { target: any }) => void;
-</script>
-
-<textarea on:change={onChange} />
