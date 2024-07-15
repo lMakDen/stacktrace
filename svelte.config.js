9874635e960fe5049883b1842005c340fe2ec77b
Zolotukhin <zolotukhin.ivan@airslate.com>
2024-07-15 11:40:40 +0300

    add deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,40 @@
 import adapter from "@sveltejs/adapter-static";
-import preprocess from "svelte-preprocess";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-export default {
-  preprocess: preprocess(),
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+
   kit: {
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: null,
-      strict: false,
-    }),
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter(),
     paths: {
       base: process.env.NODE_ENV === "production" ? "/stacktrace" : "",
     },
   },
 };
+
+export default config;
+
+// import adapter from "@sveltejs/adapter-static";
+// import preprocess from "svelte-preprocess";
+
+// export default {
+//   preprocess: preprocess(),
+//   kit: {
+//     adapter: adapter({
+//       pages: "build",
+//       assets: "build",
+//       fallback: null,
+//       strict: false,
+//     }),
+//     paths: {
+//       base:
+//         process.env.NODE_ENV === "production" ? "/stacktrace.github.io" : "",
+//     },
+//   },
+// };
